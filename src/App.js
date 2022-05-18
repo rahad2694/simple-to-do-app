4fc682169f44d1249b93e8fccbfe945b5c05ef32
rahad2694 <rahad.haw@gmail.com>
2022-05-19 01:43:52 +0600

    Navbar and signin page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Navbar from './components/Navbar/Navbar';
+import NotFound from './components/NotFound/NotFound';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+
+
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "cupcake",
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
